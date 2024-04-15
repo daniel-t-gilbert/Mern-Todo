c896a333fb6b470fce731c330d9c88324753dfdf
daniel-t-gilbert <danieltgilbert03@gmail.com>
2024-04-15 16:52:47 +0530

    server link added

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://mern-todo-roov.onrender.com",
         changeOrigin: true,
       },
     },
